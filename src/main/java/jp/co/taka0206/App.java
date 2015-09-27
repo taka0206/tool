613cb05daf3254784df957f1859c80258b381626
å <taka0206@me.com>
2015-09-27 10:51:24 +0900

    first commit

---
+++ src/main/java/jp/co/taka0206/App.java
@@ -0,0 +1,13 @@
+package jp.co.taka0206;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
